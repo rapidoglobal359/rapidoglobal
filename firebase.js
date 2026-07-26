3a87e3f3ffd204dd20ebaa64176803129032b241
rapidoglobal359 <rapidoglobal359@gmail.com>
2026-07-25 22:34:08 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,8 @@
-
+const firebaseConfig = {
+  apiKey: "AIzaSyD98AmOX8eHYn2QSxU8ImcRGzyDqQ9QXQs",
+  authDomain: "rapido-global.firebaseapp.com",
+  projectId: "rapido-global",
+  storageBucket: "rapido-global.firebasestorage.app",
+  messagingSenderId: "205317876725",
+  appId: "1:205317876725:web:8da4ca7164e2bc0022cebc"
+};
