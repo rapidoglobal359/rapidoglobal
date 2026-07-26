fb7bc8d04f41fef78ff61bc96917c6fe3df9ec47
rapidoglobal359 <rapidoglobal359@gmail.com>
2026-07-26 00:37:24 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,14 +2,13 @@ import { auth, db } from "./firebase.js";
 
 import {
   createUserWithEmailAndPassword
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+} from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
 
 import {
   collection,
   addDoc,
   getDocs
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
+} from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
 const btnRegistrar = document.getElementById("registrar");
 const btnPrealerta = document.getElementById("prealerta");
 
