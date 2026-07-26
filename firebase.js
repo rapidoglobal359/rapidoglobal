07d7f89158a77613294ea40af3e5b5bc3ae840ec
rapidoglobal359 <rapidoglobal359@gmail.com>
2026-07-25 22:44:37 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,19 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyD98AmOX8eHYn2QSxU8ImcRGzyDqQ9QXQs",
-  authDomain: "rapido-global.firebaseapp.com",
-  projectId: "rapido-global",
-  storageBucket: "rapido-global.firebasestorage.app",
-  messagingSenderId: "205317876725",
-  appId: "1:205317876725:web:8da4ca7164e2bc0022cebc"
-};
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyD98AmOX8eHYn2QSxU8ImcRGzyDqQ9QXQs",
+    authDomain: "rapido-global.firebaseapp.com",
+    projectId: "rapido-global",
+    storageBucket: "rapido-global.firebasestorage.app",
+    messagingSenderId: "205317876725",
+    appId: "1:205317876725:web:8da4ca7164e2bc0022cebc"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
