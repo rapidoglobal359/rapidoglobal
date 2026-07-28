6df69fc91b9fa34a955514909cb552f54e72528e
rapidoglobal359 <rapidoglobal359@gmail.com>
2026-07-27 22:20:53 -0500

    admin.js

--- admin.js
+++ admin.js
@@ -6,11 +6,12 @@ import {
 
 import {
   collection,
-  getDocs,
-  query
+  getDocs
 } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
 
+
 const listaAdmin = document.getElementById("listaAdmin");
+
 
 onAuthStateChanged(auth, async (usuario) => {
 
@@ -19,12 +20,98 @@ onAuthStateChanged(auth, async (usuario) => {
     return;
   }
 
+
   if (usuario.email !== "AlmeidaEdwin81@gmail.com") {
+
     alert("No tienes permisos para acceder al panel de administrador.");
+
     window.location.href = "cliente.html";
+
     return;
+
   }
 
-  listaAdmin.innerHTML = "<p>Cargando prealertas...</p>";
+
+  cargarPrealertas();
+
 
 });
+
+
+
+async function cargarPrealertas() {
+
+
+  listaAdmin.innerHTML =
+  "<p>Cargando prealertas...</p>";
+
+
+  try {
+
+
+    const resultado = await getDocs(
+      collection(db, "prealertas")
+    );
+
+
+    if (resultado.empty) {
+
+
+      listaAdmin.innerHTML =
+      "<p>No hay prealertas registradas.</p>";
+
+
+      return;
+
+    }
+
+
+    listaAdmin.innerHTML = "";
+
+
+    resultado.forEach((doc) => {
+
+
+      const datos = doc.data();
+
+
+      listaAdmin.innerHTML += `
+
+      <div class="tarjeta-prealerta">
+
+        <h3>📦 Tracking: ${datos.tracking}</h3>
+
+        <p>
+        <strong>Correo:</strong>
+        ${datos.correo}
+        </p>
+
+
+        <p>
+        <strong>Estado:</strong>
+        ${datos.estado || "Prealertado"}
+        </p>
+
+
+      </div>
+
+      <hr>
+
+      `;
+
+
+    });
+
+
+  } catch(error) {
+
+
+    console.log(error);
+
+    listaAdmin.innerHTML =
+    "<p>Error cargando prealertas.</p>";
+
+  }
+
+
+}
